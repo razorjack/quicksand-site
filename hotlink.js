37e7372ebe44854873d987f1e670d55b570a26bf
Jacek Galanciak <jacek.galanciak@gmail.com>
2017-07-17 16:01:21 +0200

    Add basic hotling prevention
    Geez, these developers are so irresponsible and lazy by copying and
    pasting code.

---
+++ hotlink.js
@@ -0,0 +1,2 @@
+if (window.console && window.console.log) { window.console.log("Dear Developer! Please consider hosting the JavaScript files on your own server instead of hotlinking to /quicksand. Thank you!") };
+alert("LoadError: main.js and all.js are no longer available from external domains.");
